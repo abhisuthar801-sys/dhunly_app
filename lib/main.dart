0b9b275fb8eddbbb22fecde01bf1984c566c73ef
abhisuthar801-sys <abhisuthar801@gmail.com>
2026-01-28 20:48:26 +0530

    Delete lib directory

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-void main() => runApp(const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(child: Text('Dhunly App Live', style: TextStyle(color: Colors.white, fontSize: 24))),
-      ),
-    ));
