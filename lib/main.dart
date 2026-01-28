53e0f0c8571ea789c1cc8539dfd4809c5959a314
abhisuthar801-sys <abhisuthar801@gmail.com>
2026-01-28 20:33:42 +0530

    Initialize Flutter app with basic UI

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(child: Text('Dhunly App Live', style: TextStyle(color: Colors.white, fontSize: 24))),
+      ),
+    ));
