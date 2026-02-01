cbda0f8a158f11fda0574f4490d3031bc840355a
abhisuthar801-sys <abhisuthar801@gmail.com>
2026-02-01 17:56:48 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,43 +3,34 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-void main() => runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: DhunlyApp(),
-    ));
+void main() => runApp(MaterialApp(home: DhunlyFinalCheck(), debugShowCheckedModeBanner: false));
 
-class DhunlyApp extends StatefulWidget {
+class DhunlyFinalCheck extends StatefulWidget {
   @override
-  _DhunlyAppState createState() => _DhunlyAppState();
+  _DhunlyFinalCheckState createState() => _DhunlyFinalCheckState();
 }
 
-class _DhunlyAppState extends State<DhunlyApp> {
-  final AudioPlayer _player = AudioPlayer();
+class _DhunlyFinalCheckState extends State<DhunlyFinalCheck> {
   List songs = [];
-  List filteredSongs = [];
-  bool isLoading = true;
-  var currentSong;
-  bool isPlaying = false;
+  bool loading = true;
+  String errorMsg = "";
 
   @override
   void initState() {
     super.initState();
-    fetchMusic();
+    getData();
   }
 
-  Future<void> fetchMusic() async {
+  getData() async {
     try {
-      final res = await http.get(Uri.parse("https://api.jsonsilo.com/public/69094396-e176-474c-8302-3866d56d788e"));
+      var res = await http.get(Uri.parse("https://api.jsonsilo.com/public/69094396-e176-474c-8302-3866d56d788e")).timeout(Duration(seconds: 10));
       if (res.statusCode == 200) {
-        setState(() {
-          songs = json.decode(res.body)['songs'];
-          filteredSongs = songs;
-          isLoading = false;
-        });
+        setState(() { songs = json.decode(res.body)['songs']; loading = false; });
+      } else {
+        setState(() { errorMsg = "Server Error: ${res.statusCode}"; loading = false; });
       }
     } catch (e) {
-      setState(() => isLoading = false);
+      setState(() { errorMsg = "Internet Connection Problem!"; loading = false; });
     }
   }
 
@@ -48,70 +39,23 @@ class _DhunlyAppState extends State<DhunlyApp> {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Image.asset('assets/logo.png', height: 40), // AAPKA NAYA LOGO
-        centerTitle: true,
+        title: Image.asset('assets/logo.png', height: 40, 
+          errorBuilder: (context, error, stackTrace) => Text("DHUNLY PRO", style: TextStyle(color: Colors.green))), 
+        backgroundColor: Colors.transparent,
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: "Search Songs...",
-                prefixIcon: Icon(Icons.search, color: Colors.blueAccent),
-                filled: true,
-                fillColor: Colors.white10,
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+      body: loading 
+        ? Center(child: CircularProgressIndicator(color: Colors.green))
+        : errorMsg.isNotEmpty 
+          ? Center(child: Text(errorMsg, style: TextStyle(color: Colors.red)))
+          : ListView.builder(
+              itemCount: songs.length,
+              itemBuilder: (context, i) => ListTile(
+                leading: Image.network(songs[i]['img']),
+                title: Text(songs[i]['title'], style: TextStyle(color: Colors.white)),
+                subtitle: Text(songs[i]['artist'], style: TextStyle(color: Colors.grey)),
+                onTap: () => AudioPlayer().play(UrlSource(songs[i]['url'])),
               ),
-              onChanged: (v) {
-                setState(() => filteredSongs = songs.where((s) => s['title'].toLowerCase().contains(v.toLowerCase())).toList());
-              },
             ),
-          ),
-          isLoading 
-            ? Expanded(child: Center(child: CircularProgressIndicator())) 
-            : Expanded(
-                child: ListView.builder(
-                  itemCount: filteredSongs.length,
-                  itemBuilder: (context, i) => ListTile(
-                    leading: ClipRRect(
-                      borderRadius: BorderRadius.circular(5),
-                      child: Image.network(filteredSongs[i]['img'], width: 50, height: 50, fit: BoxFit.cover),
-                    ),
-                    title: Text(filteredSongs[i]['title'], style: TextStyle(fontWeight: FontWeight.bold)),
-                    subtitle: Text(filteredSongs[i]['artist']),
-                    onTap: () {
-                      _player.play(UrlSource(filteredSongs[i]['url']));
-                      setState(() { currentSong = filteredSongs[i]; isPlaying = true; });
-                    },
-                  ),
-                ),
-              ),
-          if (currentSong != null) _miniPlayer(),
-        ],
-      ),
-    );
-  }
-
-  Widget _miniPlayer() {
-    return Container(
-      color: Colors.blueAccent.withOpacity(0.8),
-      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      child: Row(
-        children: [
-          Image.network(currentSong['img'], width: 40, height: 40),
-          SizedBox(width: 10),
-          Expanded(child: Text(currentSong['title'], overflow: TextOverflow.ellipsis)),
-          IconButton(
-            icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
-            onPressed: () {
-              isPlaying ? _player.pause() : _player.resume();
-              setState(() => isPlaying = !isPlaying);
-            },
-          )
-        ],
-      ),
     );
   }
 }
