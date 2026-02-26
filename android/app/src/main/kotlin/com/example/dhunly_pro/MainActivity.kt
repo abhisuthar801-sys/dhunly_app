d75e3250d4fd15fb4024213eed63791313db28e4
abhisuthar801-sys <abhisuthar801@gmail.com>
2026-02-26 17:46:31 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/dhunly_pro/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/dhunly_pro/MainActivity.kt
@@ -3,5 +3,6 @@ package com.example.dhunly_pro
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    // Ye khali rehna chahiye, FlutterActivity khud sab sambhaal legi
+    // Naye Flutter (V2 Embedding) mein yahan kuch bhi likhne ki zarurat nahi hai.
+    // FlutterActivity apne aap sab handle kar leti hai.
 }
